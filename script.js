41f948abc28fe21f7e466694d2a25f00cfebc3f5
Diallo <abdoulayediogodiallo23@gmail.com>
2026-07-16 15:15:44 +0000

    Premier commit - portfolio

---
+++ script.js
@@ -0,0 +1,122 @@
+
+// Changement de couleurs active
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const top = section.offsetTop - 150;
+        const bottom = top + section.offsetHeight;
+
+        if (window.scrollY >= top && window.scrollY < bottom) {
+            current = section.id;
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.toggle(
+            "active",
+            link.getAttribute("href") === `#${current}`
+        );
+    });
+});
+// Ecriture magique
+const words = [
+    "Devéloppeur Web",
+    "Passionner Par Informatique",
+    "Très polyvalent"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+const typing = document.querySelector(".typing");
+
+
+function type() {
+
+    let currentWord = words[wordIndex];
+
+    if (!deleting) {
+
+        typing.textContent = currentWord.substring(0, charIndex++);
+        
+        if (charIndex > currentWord.length) {
+            deleting = true;
+            setTimeout(type, 1500);
+            return;
+        }
+
+    } else {
+
+        typing.textContent = currentWord.substring(0, charIndex--);
+
+        if (charIndex === 0) {
+            deleting = false;
+            wordIndex++;
+
+            if (wordIndex === words.length) {
+                wordIndex = 0;
+            }
+        }
+    }
+
+
+    setTimeout(type, deleting ? 50 : 100);
+}
+
+
+type();
+
+
+// Event Listeners: Handling toggle event
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+//  Store color theme for future visits
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark"); //add this
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light"); //add this
+  }
+}
+
+// Save user preference on load
+
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+
+//Adding date
+
+let myDate = document.querySelector("#datee");
+
+const yes = new Date().getFullYear();
+myDate.innerHTML = yes;
