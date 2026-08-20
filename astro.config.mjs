0a516c29e171266c7ae5def4b805cc6aa24ded5a
Diallo <abdoulayediogodiallo23@gmail.com>
2026-08-20 19:09:09 +0000

    Ajout de la page 404, bouton WhatsApp et optimisation du SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  site: 'https://diogo23-dev.github.io',
+  site: 'https://diogo23-dev.github.io/porfolio/',
   base: '/porfolio',
+  integrations: [sitemap()],
 });
